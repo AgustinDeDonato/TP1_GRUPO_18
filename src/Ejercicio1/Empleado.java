6bb398bd12653d2cf9244dda8717332d96d5633e
Francisco Muslera <francisco.muslera@gmail.com>
2025-03-29 21:28:32 -0300

    agrego encapsulamiento y constructor punto 5

--- src/Ejercicio1/Empleado.java
+++ src/Ejercicio1/Empleado.java
@@ -11,7 +11,47 @@ package Ejercicio1;
 	    // Constructor
 	    public Empleado() {
 	        this.id = contador++;
+	        this.nombre = "sin nombre";
+	        this.edad = 99;
 	    }
+	    
+	    public Empleado(String nombre, int edad) {
+	        this.id = contadorID++;
+	        this.nombre = nombre;
+	        this.edad = edad;
+	    }
+
+		public static int getContador() {
+			return contador;
+		}
+
+		public static void setContador(int contador) {
+			Empleado.contador = contador;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public int getEdad() {
+			return edad;
+		}
+
+		public void setEdad(int edad) {
+			this.edad = edad;
+		}
+
+		public int getId() {
+			return id;
+		}
+	    
+	    
+	    
+	    
 	    
 	 
 	}
